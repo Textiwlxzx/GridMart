0e1339a993328da779b7cc2d2d1a955a3a7fc75c
GameBoBoo <Chawalertsavittrakul@gmail.com>
2025-03-08 22:41:54 +0700

    Update Button1.java

--- TestGUI/src/button/Button1.java
+++ TestGUI/src/button/Button1.java
@@ -6,6 +6,8 @@ import java.awt.geom.RoundRectangle2D;
 
 public class Button1 extends JButton{
     
+    private int radius = 0;
+    
     public Button1(){
         setOpaque(false);
         setContentAreaFilled(false);
@@ -15,13 +17,23 @@ public class Button1 extends JButton{
         setForeground(new Color(0,0,0));
         setFont(new Font("Arial", Font.BOLD, 16));
     }
+    
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+    
     @Override
     protected void paintBorder(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
                 
         g2.setColor(getForeground());
-        g2.draw(new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, 30, 30));
+        g2.setStroke(new BasicStroke(1));
+        g2.draw(new RoundRectangle2D.Float(0, 0, getWidth() -1, getHeight() -1, radius, radius));
                 
         g2.dispose();
         }
