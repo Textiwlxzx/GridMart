397230d6e2e13a783ac57c9bedf66de15d931085
67070033 Chonticha <67070033@kmitl.ac.th>
2025-03-31 23:21:59 +0700

    TestFont

---
+++ GridMart/src/CustomFont/LabelText.java
@@ -0,0 +1,15 @@
+
+package CustomFont;
+
+import java.awt.*;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class LabelText extends JLabel {
+    
+    public LabelText() {
+        setForeground(new Color(54, 54, 54));
+        setFont(new Font("Afacad", Font.PLAIN, 24));
+    }
+    
+}
