f82c6a440a356dc30589db12061b71227fa1d7ed
67070033 Chonticha <67070033@kmitl.ac.th>
2025-03-31 23:25:36 +0700

    rectangle

---
+++ GridMart/src/Rectangle/Rectangle1.java
@@ -0,0 +1,34 @@
+package Rectangle;
+
+import Rectangle.*;
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class Rectangle1 extends JPanel{
+    
+    private int radius = 0;
+    
+    public Rectangle1(){
+        setOpaque(false);
+    }
+    
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2.setColor(new Color (255, 255, 255,127));
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+        
+        super.paintComponent(g);
+    }
+
+}
---
+++ GridMart/src/Rectangle/RectangleOrder.java
@@ -0,0 +1,34 @@
+package Rectangle;
+
+import Rectangle.*;
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class RectangleOrder extends JPanel{
+    
+    private int radius = 0;
+    
+    public RectangleOrder(){
+        setOpaque(false);
+    }
+    
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2.setColor(new Color (206, 192, 235));
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+        
+        super.paintComponent(g);
+    }
+
+}
